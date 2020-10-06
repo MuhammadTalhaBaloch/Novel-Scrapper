07cd6af3d059498ad1772ea87b2e2bd78db10d06
Muhammad Talha <39367862+MuhammadTalhaBaloch@users.noreply.github.com>
2020-10-06 22:28:03 +0500

    Add files via upload

---
+++ NovelList.py
@@ -0,0 +1,52 @@
+import requests
+from bs4 import BeautifulSoup
+    
+import re
+
+
+TAG_RE = re.compile(r'<[^>]+>')
+
+def remove_tags(text):
+    return TAG_RE.sub('', text)
+def insertChar(mystring, position, chartoinsert ):
+    longi = len(mystring)
+    mystring   =  mystring[:position] + chartoinsert + mystring[position:] 
+    return mystring  
+
+session = requests.Session()
+headers = {'User-Agent': 'Opera/12.02 (Android 4.1; Linux; Opera Mobi/ADR-1111101157; U; en-US) Presto/2.9.201 Version/12.02'}
+
+
+#... whatever requests config you need here
+
+ #x=149
+WebUrl='https://www.readlightnovel.org/novel-list'
+#print(WebUrl)
+response = session.get(WebUrl,headers=headers)
+url = response.url
+page = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+results = soup.find_all('div', class_ = 'list-by-word-body')
+first_movie = results[0]
+
+phela=first_movie.ul
+
+litag=phela.find_all("li")
+#print(litag[4].text)
+#print(litag[4].a.text)
+
+for i in range(len(litag)):
+    if i % 2 == 0: 
+            print("Name:{0}:  \n".format(litag[i].a.text))
+            print("Url:{0}:  \n".format(litag[i].a.get('href')))
+
+#for tag in phela.find_all("li"):
+        
+
+
+#print('\n')
+#print(first_movie.a.text)
+#print('\n')
+#print(first_movie.a)
+#print('\n')
+#print(first_movie.div.p)
